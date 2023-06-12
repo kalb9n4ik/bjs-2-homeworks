f59e58d4102d03a5f7fb2b692980c030f26298ad
Николай Котко <kalb9n4ik@gmail.com>
2023-06-13 00:00:39 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,28 @@
 function compareArrays(arr1, arr2) {
-  
+
+  let check = arr1.every(function(elem, index, arr) {
+  	if (arr1.length === arr2.length) {
+		if (elem === arr2[index]) {
+		return true;
+		} else {
+		return false;
+		}
+  	} else {
+  		return false;
+  	}
+		
+	});
+  return check;
+  	
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	
+  let arrayUsers = users.filter(user => user.gender === gender);
+  let arrayAge = arrayUsers.map(user => user.age);
+  let sumAge = arrayAge.reduce(function(a, b) {
+	  return a + b;
+	});
+
+  return sumAge / arrayAge.length;
 }
