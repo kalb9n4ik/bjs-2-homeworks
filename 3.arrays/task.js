df1c7b6b71e4e678a32ad1159b2b14a2c9bbaea6
Николай Котко <kalb9n4ik@gmail.com>
2023-06-21 23:13:01 +0300

    new  solution

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,28 +1,24 @@
 function compareArrays(arr1, arr2) {
-
-  let check = arr1.every(function(elem, index, arr) {
-  	if (arr1.length === arr2.length) {
-		if (elem === arr2[index]) {
-		return true;
-		} else {
-		return false;
-		}
-  	} else {
-  		return false;
-  	}
-		
-	});
-  return check;
-  	
+	let isSame;
+	if (arr1.length !== arr2.length) {
+		isSame = false;
+	} else {
+	 isSame = arr1.every((elem, index) => elem === arr2[index])
+	}
+  
+  return isSame
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-	
-  let arrayUsers = users.filter(user => user.gender === gender);
-  let arrayAge = arrayUsers.map(user => user.age);
-  let sumAge = arrayAge.reduce(function(a, b) {
-	  return a + b;
-	});
+ const filterUsers = users.filter((item) => item.gender === gender).map((item) => item.age);
 
-  return sumAge / arrayAge.length;
+	if (filterUsers.length === 0) {
+		return 0;
+	}
+
+const totalAge = filterUsers.reduce((currentSum, currentNumber) => currentSum + currentNumber, 0);
+const averageAge = totalAge / filterUsers.length;
+
+return averageAge;
+
 }
