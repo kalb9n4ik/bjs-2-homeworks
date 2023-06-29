0eff2574bb22186f3dbd09ba912451f9c54ad89f
Николай Котко <kalb9n4ik@gmail.com>
2023-06-30 01:43:00 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,86 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+
+		if(arguments.length < 2 || time === null) {
+			throw new Error('Отсутствуют обязательные аргументы')
+		}
+
+		if(this.alarmCollection.includes(time)) {
+			console.warn('Уже присутствует звонок на это же время')
+		}
+
+		this.alarmCollection.push({callback: callback, time: time, canCall: true})
+
+	}
+
+	removeClock(delTime) {
+
+		const filteredAlarm = this.alarmCollection.filter(function (n) {
+			return n.time !== delTime
+		})
+
+		this.alarmCollection = filteredAlarm;
+	
+	}
+
+	getCurrentFormattedTime() {
+
+		let Data = new Date();
+		let Hour = Data.getHours();
+		let Minutes = Data.getMinutes();
+
+		if(Hour < 10) {
+			Hour = "0" + Hour
+		}
+
+		if(Minutes < 10) {
+			Minutes = "0" + Minutes
+		}
+
+		return `${Hour}:${Minutes}`
+	}
+
+	start() {
+
+		if(this.intervalId) {
+			return;
+		}
+
+		this.intervalId = setInterval(() => {
+
+			this.alarmCollection.forEach((alarm) => {
+				if(alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+					alarm.canCall = false
+					// вызвать колбэк звонка
+					alarm.callback()
+				}
+			})
+
+			
+		}, 1000);
+
+	}
+
+	stop() {
+		clearInterval(this.intervalId)
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+
+		this.alarmCollection.forEach((alarm) => {
+			alarm.canCall = true;
+		})
+	}
+
+	clearAlarms() {
+		this.stop()
+		this.alarmCollection = []
+	}
+
+}
